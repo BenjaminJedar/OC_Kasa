330fa2f289a76012a413629ebcd2a48b0aedf884
BenjaminJedar <benjisaintpaul@hotmail.fr>
2023-11-01 22:42:17 +0100

    Finalisation du projet

--- src/pages/Error/error.jsx
+++ src/pages/Error/error.jsx
@@ -4,8 +4,8 @@ import './error.css';
 function Error() {
   return (
     <div className="div_error">
-      <h1>404</h1>
-      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <h1 className="title_404">404</h1>
+      <p className="msg_404">Oups! La page que vous demandez n'existe pas.</p>
       <Link to="/" className="lien_accueil_404">
         Retourner sur la page d’accueil
       </Link>
