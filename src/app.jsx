5adbd4aa2ae1d7136ebfa04e5aa8e267c50a6282
BenjaminJedar <benjisaintpaul@hotmail.fr>
2024-02-02 21:38:35 +0100

    Remplacement de BrowserRouter par Hashrouter

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/home';
 import FicheLogement from './pages/Fiche_logement/fiche_logement';
 import APropos from './pages/A_propos/a_propos';
